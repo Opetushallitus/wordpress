7c9cda6615d7828d9df3ba7057bd614d1bba6606
Anniina Salmi <anniina.salmi@cybercom.com>
2014-01-07 14:23:56 +0200

    Changed notifications breadcrum slug

--- Oppija/wp-content/themes/ophver3/functions.php
+++ Oppija/wp-content/themes/ophver3/functions.php
@@ -553,7 +553,7 @@ function create_post_type_html5()
     register_post_type('oph-notification', // Register Custom Post Type
         array(
         'labels' => array(
-            'name' => __('Huomautukset', 'html5blank'), // Rename these to suit
+            'name' => __('Tiedoksi', 'html5blank'), // Rename these to suit
             'singular_name' => __('Notification', 'html5blank'),
             'add_new' => __('Add New', 'html5blank'),
             'add_new_item' => __('Add New Notification', 'html5blank'),
