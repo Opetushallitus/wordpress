028d632d1a92f0f3b2f0c1ae15160d3011961f53
Anniina Salmi <anniina.salmi@cybercom.com>
2014-01-13 16:43:47 +0200

    Disabled condizionir

--- Oppija/wp-content/themes/ophver3/header.php
+++ Oppija/wp-content/themes/ophver3/header.php
@@ -51,7 +51,7 @@
 	<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/matchmedia.js"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/respond.min.js"></script>
 
-	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/selectivizr.min.js"></script>
 	<![endif]-->
 	
