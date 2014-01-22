e8b7b35d8e3309e981bc711cc28e6031eeda4317
Anniina Salmi <anniina.salmi@cybercom.com>
2014-01-22 11:57:40 +0200

    Updated Virkailijan työpöytä's sidebar functions and added styles

--- Virkailija/wp-content/themes/oph-virkailija/sidebar-left.php
+++ Virkailija/wp-content/themes/oph-virkailija/sidebar-left.php
@@ -10,29 +10,14 @@
                     <h3><?php _e('Ohjeet ja materiaalit', 'oph') ?></h3>
                 </div>
                 <div id="sidebar-listing">
-                    <ul class="sidebar-list">
-                                <?php 	/* Widgetized sidebar, if you have the plugin installed. */
-                                                if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Left sidebar') ) : ?>
+                    <ul class="sidebar-list-content">
+                        <?php /* Widgetized sidebar, if you have the plugin installed. */
+                            if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Left sidebar') ) : ?>
 
-                            <?php
-                                $args=array(
-                                'showposts'=>5,
-                                'post_type' => 'page',
-                                'caller_get_posts'=>1
-                                );
-                             $my_query = new WP_Query($args);
-                             if( $my_query->have_posts() ) {
-                               while ($my_query->have_posts()) : $my_query->the_post(); ?>
-                                <ul>
-                                    <li><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></li>
-                                    <li><small><?php the_time('j.n.Y') ?></small></li>
-                                </ul>
-                                 <?php
-                               endwhile;
-                             } ?>
-
-                                <?php endif; ?>
-                        </ul>
+                        <?php wp_list_pages('sort_column=post_date&show_date=created&title_li='); ?>
+                    
+                    </ul>
+                        <?php endif; ?>
                 </div>
             </div>
 	</div><!-- end sidebar -->
