ab3aeb2e80fb6e6aff816723287e47e4d73d7d15
Anniina Salmi <anniina.salmi@cybercom.com>
2014-04-30 13:00:02 +0300

    Remove useless folder

--- Oppija/wp-content/themes/ophver31/js/excerpt_counter.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function() {
-    if(jQuery('#excerpt').length) {
-        jQuery('#postexcerpt .handlediv').after('<div style="position:absolute;top:0px;right:5px;color:#666;"><small>Excerpt length: </small><input type="text" value="0" maxlength="3" size="3" id="excerpt_counter" readonly="" style="background:#fff;"> <small>character(s).</small></div>');
-        jQuery('#excerpt_counter').val(jQuery('#excerpt').val().length);
-        jQuery('#excerpt').keyup( function() {
-            jQuery('#excerpt_counter').val(jQuery('#excerpt').val().length);
-        });
-    }
-});
